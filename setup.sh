7290cc7195bb8f78852fdfca6339313018d6d66f
Kanyerezi30 <kanyerezi30@gmail.com>
2025-03-20 12:58:34 +0300

    added installation setup

--- setup.sh
+++ setup.sh
@@ -1,3 +1,8 @@
 #!/usr/bin/env bash
 
-mv 
+pref_conda=$(echo $CONDA_PREFIX)
+#cp ${HOME}/VaricellaGen/bin/VaricellaGen ${pref_conda}/envs/VaricellaGen/bin/
+cp ${HOME}/VaricellaGen/bin/process_gvcf.py ${pref_conda}/envs/VaricellaGen/bin/
+
+echo "export PATH=${HOME}/VaricellaGen/bin:$PATH" >>~/.bashrc
+source ~/.bashrc
